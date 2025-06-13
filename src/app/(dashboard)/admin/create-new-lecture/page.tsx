b92eb6bbd5c3da06e6231386acef5f4df347c795
Yuht <nguyenthithuy1022003@gmail.com>
2025-06-13 17:34:26 +0700

    add: implement UI for adin create lecturer account

--- src/app/(dashboard)/admin/create-new-lecture/page.tsx
+++
@@ -1,9 +0,0 @@
-import CreateNewLecture from '@/components/features/admin/CreateNewLecture';
-
-export default function AdminCreateNewLecturePage() {
-	return (
-		<div className="p-6">
-			<CreateNewLecture />
-		</div>
-	);
-}
---
+++ src/app/(dashboard)/admin/create-new-lecturer/page.tsx
@@ -0,0 +1,9 @@
+import CreateNewLecturer from '@/components/features/admin/CreateNewLecturer';
+
+export default function AdminCreateNewLecturePage() {
+	return (
+		<div>
+			<CreateNewLecturer />
+		</div>
+	);
+}
