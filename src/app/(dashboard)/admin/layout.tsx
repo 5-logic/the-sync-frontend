6ba093d6875228cc62c03af6640e95fcb5903698
Quốc Chương <quocchuong2609@gmail.com>
2025-06-17 10:23:25 +0700

    feat: add metadata export for Admin Dashboard layout

--- src/app/(dashboard)/admin/layout.tsx
+++ src/app/(dashboard)/admin/layout.tsx
@@ -1,5 +1,12 @@
+import { createMetadata } from '@/app/metadata';
 import CollapsibleLayout from '@/components/layout/CollapsibleLayout';
 import AdminSidebar from '@/components/layout/Sidebar/AdminSidebar';
+
+export const metadata = createMetadata({
+	title: 'Admin Dashboard',
+	description:
+		'Admin Dashboard for TheSync - Group Formation and Capstone Thesis Development',
+});
 
 export default function AdminLayout({
 	children,
