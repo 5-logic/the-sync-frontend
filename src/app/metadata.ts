a513264d9b06a160910e777bd2f5d273b4413621
Quốc Chương <quocchuong2609@gmail.com>
2025-06-17 09:49:43 +0700

    feat: add metadata configuration for TheSync application

---
+++ src/app/metadata.ts
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'TheSync',
+	description: 'Group Formation and Capstone Thesis Development',
+	icons: {
+		icon: '/icons/TheSyncLogo.ico',
+		shortcut: '/icons/TheSyncLogo.ico',
+	},
+};
