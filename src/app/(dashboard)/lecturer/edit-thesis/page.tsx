76836d7fb9efd12120cd28915d5dd72482b2c6f3
Yuht <nguyenthithuy1022003@gmail.com>
2025-06-21 11:01:13 +0700

    refactor: simplify LecturerThesisCreatePage and LecturerThesisEditPage by removing Layout wrapper

--- src/app/(dashboard)/lecturer/edit-thesis/page.tsx
+++ src/app/(dashboard)/lecturer/edit-thesis/page.tsx
@@ -1,17 +1,7 @@
 'use client';
 
-import { Layout } from 'antd';
-
 import EditThesis from '@/components/features/lecturer/EditThesis';
 
-const { Content } = Layout;
-
 export default function LecturerThesisEditPage() {
-	return (
-		<Layout style={{ minHeight: '100vh', background: 'transparent' }}>
-			<Content style={{ padding: '24px' }}>
-				<EditThesis />
-			</Content>
-		</Layout>
-	);
+	return <EditThesis />;
 }
