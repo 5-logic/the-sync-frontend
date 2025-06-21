d489f880a9564be3b3eec0137f2e31afc31e6137
Yuht <nguyenthithuy1022003@gmail.com>
2025-06-21 11:06:32 +0700

    update: update layout

--- src/app/(dashboard)/lecturer/create-thesis/page.tsx
+++ src/app/(dashboard)/lecturer/create-thesis/page.tsx
@@ -1,17 +1,7 @@
 'use client';
 
-import { Layout } from 'antd';
-
 import CreateThesis from '@/components/features/lecturer/CreateThesis';
 
-const { Content } = Layout;
-
 export default function LecturerThesisCreatePage() {
-	return (
-		<Layout style={{ minHeight: '100vh', background: 'transparent' }}>
-			<Content style={{ padding: '24px' }}>
-				<CreateThesis />
-			</Content>
-		</Layout>
-	);
+	return <CreateThesis />;
 }
