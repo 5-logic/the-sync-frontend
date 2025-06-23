1b723644522ae8f3c44e0075e62918c82591b414
Quốc Chương <quocchuong2609@gmail.com>
2025-06-23 08:44:59 +0700

    refactor: add inferred type for LecturerToggleStatusSchema

--- src/schemas/lecturer.ts
+++ src/schemas/lecturer.ts
@@ -19,7 +19,13 @@ export const LecturerUpdateSchema = LecturerSchema.pick({
 	isModerator: true,
 });
 
+export const LecturerToggleStatusSchema = z.object({
+	isActive: z.boolean().optional(),
+	isModerator: z.boolean().optional(),
+});
+
 // Export inferred types
 export type Lecturer = z.infer<typeof LecturerSchema>;
 export type LecturerCreate = z.infer<typeof LecturerCreateSchema>;
 export type LecturerUpdate = z.infer<typeof LecturerUpdateSchema>;
+export type LecturerToggleStatus = z.infer<typeof LecturerToggleStatusSchema>;
