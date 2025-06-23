f6fdaf7fde39126e4affe879fc2ffa32e4e6df11
Yuht <nguyenthithuy1022003@gmail.com>
2025-06-23 21:22:58 +0700

    update: update pagination

--- src/components/common/TablePagination/TablePagination.tsx
+++ src/components/common/TablePagination/TablePagination.tsx
@@ -5,7 +5,7 @@ import type { TablePaginationConfig } from 'antd/es/table';
 const TablePagination: TablePaginationConfig = {
 	showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
 	showSizeChanger: true,
-	pageSizeOptions: ['5', '10', '20', '50'],
+	pageSizeOptions: ['10', '20', '50', '100'],
 	defaultPageSize: 10,
 };
 
