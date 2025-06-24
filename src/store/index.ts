bfdb9c035c6c4a58bd9d13a7b15f8fb62d6f56f5
Quốc Chương <quocchuong2609@gmail.com>
2025-06-24 10:34:57 +0700

    feat: add lecturer store for managing lecturer data and actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 // Store exports
 export { useAppStore } from '@/store/useAppStore';
-export { useMilestoneStore } from '@/store/useMilestoneStore';
-export { useStudentStore } from '@/store/useStudentStore';
+export { useLecturerStore } from '@/store/useLecturerStore';
 export { useMajorStore } from '@/store/useMajorStore';
+export { useMilestoneStore } from '@/store/useMilestoneStore';
 export { useSemesterStore } from '@/store/useSemesterStore';
+export { useStudentStore } from '@/store/useStudentStore';
