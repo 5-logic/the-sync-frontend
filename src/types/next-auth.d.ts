e54ccd9eee3f8996b9ffba963603ea80b0b589c2
Thunder <htn10a2@gmail.com>
2025-06-28 16:59:18 +0700

    refactor: add rememberMe property to NextAuth types for enhanced session management

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -22,6 +22,7 @@ declare module 'next-auth' {
 		} & DefaultSession['user'];
 		accessToken?: string;
 		refreshToken?: string;
+		rememberMe?: boolean;
 	}
 	interface User {
 		id: string;
@@ -30,6 +31,7 @@ declare module 'next-auth' {
 		isModerator?: boolean;
 		accessToken?: string;
 		refreshToken?: string;
+		rememberMe?: boolean;
 		// Common user fields
 		fullName?: string;
 		email?: string;
@@ -54,6 +56,7 @@ declare module 'next-auth/jwt' {
 		accessToken?: string;
 		refreshToken?: string;
 		accessTokenExpires?: number;
+		rememberMe?: boolean;
 		// Common user fields (UserSchema)
 		fullName?: string;
 		email?: string;
