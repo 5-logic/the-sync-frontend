89327d7d377f025a1707c2cbe4829af7635aa4f4
Thunder <htn10a2@gmail.com>
2025-07-03 15:31:23 +0700

    refactor: remove unused export for usePublishTheses from thesis hooks

--- src/hooks/thesis/index.ts
+++ src/hooks/thesis/index.ts
@@ -1,4 +1,3 @@
 export { useThesisDetail } from '@/hooks/thesis/useThesisDetail';
 export { useThesisActions } from '@/hooks/thesis/useThesisActions';
 export { useThesisForm } from '@/hooks/thesis/useThesisForm';
-export { usePublishTheses } from '@/hooks/thesis/usePublishTheses';
