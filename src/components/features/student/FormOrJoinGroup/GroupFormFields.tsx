4771482cb2db6a4f68297d748eea84f4bf28f58b
Thunder <htn10a2@gmail.com>
2025-07-06 09:16:16 +0700

    fix: correct fallback operator in skill tree data mapping

--- src/components/features/student/FormOrJoinGroup/GroupFormFields.tsx
+++ src/components/features/student/FormOrJoinGroup/GroupFormFields.tsx
@@ -57,7 +57,7 @@ export default function GroupFormFields() {
 				set.skills?.map((skill) => ({
 					value: skill.id,
 					title: skill.name,
-				})) || [],
+				})) ?? [],
 		}));
 	}, [skillSets]);
 
