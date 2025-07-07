0c2283d749fd6ba605395c2da58f845953225ee0
Thunder <htn10a2@gmail.com>
2025-07-07 18:16:56 +0700

    feat: add index file for GroupRequestsDialog component exports

---
+++ src/components/features/student/GroupDashboard/GroupRequestsDialog/index.ts
@@ -0,0 +1,11 @@
+export { default as InviteActions } from '@/components/features/student/GroupDashboard/GroupRequestsDialog/InviteActions';
+export { default as InviteRequestsTable } from '@/components/features/student/GroupDashboard/GroupRequestsDialog/InviteRequestsTable';
+export { default as JoinActions } from '@/components/features/student/GroupDashboard/GroupRequestsDialog/JoinActions';
+export { default as JoinRequestsTable } from '@/components/features/student/GroupDashboard/GroupRequestsDialog/JoinRequestsTable';
+export { default as RequestsSearchControls } from '@/components/features/student/GroupDashboard/GroupRequestsDialog/RequestsSearchControls';
+export { default as RequestsTab } from '@/components/features/student/GroupDashboard/GroupRequestsDialog/RequestsTab';
+export { default as RequestStatusTag } from '@/components/features/student/GroupDashboard/GroupRequestsDialog/RequestStatusTag';
+export { default as StudentInfo } from '@/components/features/student/GroupDashboard/GroupRequestsDialog/StudentInfo';
+
+// Re-export the main component for convenience
+export { default } from '@/components/features/student/GroupDashboard/GroupRequestsDialog';
