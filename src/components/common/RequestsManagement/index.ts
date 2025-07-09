f45b5a7cc381bde47d3c899fff2637fe4f669aa1
Thunder <htn10a2@gmail.com>
2025-07-09 09:50:29 +0700

    feat: create RequestsManagement index file to export components

---
+++ src/components/common/RequestsManagement/index.ts
@@ -0,0 +1,9 @@
+export { default as RequestsButton } from './RequestsButton';
+export { default as RequestsDialog } from './RequestsDialog';
+export { default as RequestsTab } from './RequestsTab';
+export { default as RequestsTable } from './RequestsTable';
+export { default as RequestsActions } from './RequestsActions';
+export { default as RequestsSearchControls } from './RequestsSearchControls';
+export { default as RequestStatusTag } from './RequestStatusTag';
+export { default as StudentInfo } from './StudentInfo';
+export * from './types';
