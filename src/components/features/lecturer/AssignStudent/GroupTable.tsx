bdb896c0abdcb7f7cad165a9584b2b5f34eff3f6
Thunder <htn10a2@gmail.com>
2025-07-12 11:15:29 +0700

    feat: add CustomGroupTable component for displaying group data in a table format

---
+++ src/components/features/lecturer/AssignStudent/GroupTable.tsx
@@ -0,0 +1,24 @@
+import { Table } from 'antd';
+import { ColumnsType } from 'antd/es/table';
+
+import { TablePagination } from '@/components/common/TablePagination';
+import { Group } from '@/lib/services/groups.service';
+
+interface CustomGroupTableProps {
+	data: Group[];
+	columns: ColumnsType<Group>;
+}
+
+export default function CustomGroupTable({
+	data,
+	columns,
+}: CustomGroupTableProps) {
+	return (
+		<Table
+			dataSource={data}
+			columns={columns}
+			rowKey="id"
+			pagination={TablePagination}
+		/>
+	);
+}
