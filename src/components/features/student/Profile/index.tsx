44d855a26276dfd09d751821e7b15f2cf2facd14
Yuht <nguyenthithuy1022003@gmail.com>
2025-07-14 11:39:39 +0700

    update: header common

--- src/components/features/student/Profile/index.tsx
+++ src/components/features/student/Profile/index.tsx
@@ -1,15 +1,18 @@
 'use client';
 
-import { Space, Typography } from 'antd';
+import { Space } from 'antd';
 
+import { Header } from '@/components/common/Header';
 import StudentProfileLayout from '@/components/features/student/Profile/StudentProfileLayout';
 import StudentProfileSections from '@/components/features/student/Profile/StudentProfileSections';
 
 export default function StudentProfilePage() {
 	return (
 		<Space direction="vertical" size="large" style={{ width: '100%' }}>
-			<Typography.Title level={2}>Student Profile</Typography.Title>
-
+			<Header
+				title="Student Profile"
+				description="Easily view and update student information including contact details, enrollment history, and performance stats"
+			/>
 			<StudentProfileLayout />
 			<StudentProfileSections />
 		</Space>
