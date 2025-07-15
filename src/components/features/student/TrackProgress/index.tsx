34ef13158b786a82e2973538c8411f435aaedadb
Yuhtnguyen <nguyenthithuy1022003@gmail.com>
2025-07-15 13:53:21 +0700

    add: track progress

--- src/components/features/student/TrackProgress/index.tsx
+++ src/components/features/student/TrackProgress/index.tsx
@@ -14,8 +14,12 @@ export default function ProjectProgressPage() {
 			style={{ width: '100%', padding: '16px 24px' }}
 		>
 			<Row gutter={16}>
-				<Col xs={24} lg={16}>
+				<Col span={24}>
 					<MilestoneStep />
+				</Col>
+			</Row>
+			<Row gutter={16}>
+				<Col xs={24} lg={16}>
 					<MilestoneDetailCard />
 				</Col>
 				<Col xs={24} lg={8}>
