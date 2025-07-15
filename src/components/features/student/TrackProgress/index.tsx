ed513f0f87e43cb57da7b89f25a98371701343ff
Yuhtnguyen <nguyenthithuy1022003@gmail.com>
2025-07-15 11:48:45 +0700

    update: use component from lecturer

--- src/components/features/student/TrackProgress/index.tsx
+++ src/components/features/student/TrackProgress/index.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+
+import ProgressOverviewCard from '@/components/features/lecturer/GroupProgess/ProgressOverviewCard';
+import { FullMockGroup } from '@/data/group';
+
 import MilestoneDetailCard from './MilestoneDetailCard';
 import MilestoneStep from './MilestoneStep';
-import ProgressOverviewCard from './ProgressOverviewCard';
 
 export default function ProjectProgressPage() {
+	const [selectedGroup] = useState<FullMockGroup | undefined>(undefined);
+
 	return (
 		<div className="p-4 md:p-6 grid grid-cols-1 lg:grid-cols-3 gap-4">
 			<div className="lg:col-span-2">
@@ -10,7 +16,7 @@ export default function ProjectProgressPage() {
 				<MilestoneDetailCard />
 			</div>
 			<div>
-				<ProgressOverviewCard />
+				{selectedGroup && <ProgressOverviewCard group={selectedGroup} />}
 			</div>
 		</div>
 	);
