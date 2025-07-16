29a36020999efd2315a6861c6d8697fb644f14ec
Yuhtnguyen <nguyenthithuy1022003@gmail.com>
2025-07-16 14:11:46 +0700

    fix: refactor StatCard layout for improved structure and alignment

--- src/components/features/lecturer/Dashboard/StatCard.tsx
+++ src/components/features/lecturer/Dashboard/StatCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Row, Space, Typography } from 'antd';
+import { Card, Col, Row, Typography } from 'antd';
 import React from 'react';
 
 const { Title, Text } = Typography;
@@ -11,20 +11,21 @@ interface StatCardProps {
 
 const StatCard: React.FC<StatCardProps> = ({ value, title, icon }) => {
 	return (
-		<Card hoverable bodyStyle={{ padding: 0 }}>
-			<Row style={{ height: 100, padding: 16 }} align="middle">
-				<Col flex="auto">
-					<Space direction="vertical" size={0}>
-						<Title level={3} style={{ margin: 0 }}>
-							{value}
-						</Title>
-						<Text type="secondary">{title}</Text>
-					</Space>
-				</Col>
-				<Col>
-					<Space>{icon}</Space>
-				</Col>
-			</Row>
+		<Card hoverable styles={{ body: { padding: 0 } }}>
+			<Col style={{ padding: 16 }}>
+				<Row align="middle" justify="space-between">
+					<Title level={3} style={{ margin: 0 }}>
+						{value}
+					</Title>
+					<span style={{ fontSize: 28 }}>{icon}</span>
+				</Row>
+
+				<Row>
+					<Text type="secondary" style={{ fontSize: 14 }}>
+						{title}
+					</Text>
+				</Row>
+			</Col>
 		</Card>
 	);
 };
