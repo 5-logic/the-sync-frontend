bcee056bd48f9029e24c5e5c37138af3512046e5
Yuhtnguyen <nguyenthithuy1022003@gmail.com>
2025-07-16 17:38:57 +0700

    add: implement Lecturer Dashboard component

---
+++ src/components/features/lecturer/DashboardModerator/index.tsx
@@ -0,0 +1,11 @@
+import { createMetadata } from '@/app/metadata';
+
+export const metadata = createMetadata({
+	title: 'Morderator Dashboard',
+	description:
+		'Morderator Dashboard for TheSync - Group Formation and Capstone Thesis Development',
+});
+
+export default function LecturerDashboardPage() {
+	// return <DashboardModerator />;
+}
