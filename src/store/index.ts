297aa5763603121abf2cb7dd29271058878c8346
Quốc Chương <quocchuong2609@gmail.com>
2025-07-17 20:42:24 +0700

    feat: update store exports to include useAssignSupervisorStore and reorder useThesisStore

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 // Store exports
 export { useAdminStore } from '@/store/useAdminStore';
 export { useAppStore } from '@/store/useAppStore';
+export { useAssignSupervisorStore } from '@/store/useAssignSupervisorStore';
 export { useGroupDashboardStore } from '@/store/useGroupDashboardStore';
 export { useGroupsStore } from '@/store/useGroupsStore';
 export { useLecturerStore } from '@/store/useLecturerStore';
@@ -12,6 +13,6 @@ export { useResponsibilityStore } from '@/store/useResponsibilityStore';
 export { useSemesterStore } from '@/store/useSemesterStore';
 export { useSkillSetStore } from '@/store/useSkillSetStore';
 export { useStudentStore } from '@/store/useStudentStore';
-export { useThesisStore } from '@/store/useThesisStore';
+export { useSubmissionStore } from '@/store/useSubmissionStore';
 export { useSupervisionStore } from '@/store/useSupervisionStore';
-export { useAssignSupervisorStore } from '@/store/useAssignSupervisorStore';
+export { useThesisStore } from '@/store/useThesisStore';
