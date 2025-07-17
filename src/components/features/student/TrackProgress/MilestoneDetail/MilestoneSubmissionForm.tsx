deb5cbe5688e108c07edb23f05f985909b36f0ee
Thunder <htn10a2@gmail.com>
2025-07-17 18:42:07 +0700

    feat: add MilestoneSubmissionForm component for milestone document submissions

---
+++ src/components/features/student/TrackProgress/MilestoneDetail/MilestoneSubmissionForm.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { Space } from 'antd';
+
+import { FileUploadSection } from './FileUploadSection';
+import { SubmissionActions } from './SubmissionActions';
+import { SubmissionWarning } from './SubmissionWarning';
+
+interface UploadInfo {
+	fileList: Array<{
+		originFileObj?: File;
+		name: string;
+	}>;
+}
+
+interface MilestoneSubmissionFormProps {
+	files: File[];
+	canSubmit: boolean;
+	isSubmitting: boolean;
+	isUpdateMode: boolean;
+	hasSubmittedDocuments: boolean;
+	submissionMessage: string;
+	onFileChange: (info: UploadInfo) => void;
+	onRemoveFile: (fileName: string, fileSize: number) => void;
+	onCancelUpdate: () => void;
+	onSubmit: () => void;
+}
+
+export function MilestoneSubmissionForm({
+	files,
+	canSubmit,
+	isSubmitting,
+	isUpdateMode,
+	hasSubmittedDocuments,
+	submissionMessage,
+	onFileChange,
+	onRemoveFile,
+	onCancelUpdate,
+	onSubmit,
+}: MilestoneSubmissionFormProps) {
+	return (
+		<Space direction="vertical" size={8} style={{ width: '100%' }}>
+			<SubmissionWarning message={submissionMessage} canSubmit={canSubmit} />
+
+			<FileUploadSection
+				files={files}
+				canSubmit={canSubmit}
+				isSubmitting={isSubmitting}
+				onFileChange={onFileChange}
+				onRemoveFile={onRemoveFile}
+			/>
+
+			<SubmissionActions
+				hasFiles={files.length > 0}
+				isUpdateMode={isUpdateMode}
+				canSubmit={canSubmit}
+				isSubmitting={isSubmitting}
+				hasSubmittedDocuments={hasSubmittedDocuments}
+				onCancel={onCancelUpdate}
+				onSubmit={onSubmit}
+			/>
+		</Space>
+	);
+}
