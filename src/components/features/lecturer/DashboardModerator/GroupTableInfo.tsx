2ea386512f98821b4673c60d4bd046cf37b3a1a4
Yuhtnguyen <nguyenthithuy1022003@gmail.com>
2025-07-18 14:45:37 +0700

    refactor: enhance structure and readability in GroupInfo component

--- src/components/features/lecturer/DashboardModerator/GroupTableInfo.tsx
+++ src/components/features/lecturer/DashboardModerator/GroupTableInfo.tsx
@@ -1,5 +1,5 @@
 import { DownloadOutlined, SearchOutlined } from '@ant-design/icons';
-import { Button, Card, Input, Select, Table } from 'antd';
+import { Button, Card, Input, Select, Space, Table, Typography } from 'antd';
 
 import { extendedGroups } from '@/data/group';
 
@@ -18,33 +18,39 @@ const groupTableData = extendedGroups.map((group) => ({
 	semester: group.semesterId,
 }));
 
+const { Title, Text } = Typography;
 export function GroupInfo() {
 	return (
 		<Card>
-			<div className="bg-white p-4 rounded shadow">
-				<h3 className="text-lg font-semibold mb-2">
-					Groups with Assigned Thesis Topics
-				</h3>
-				<div className="flex justify-between items-center mb-4">
-					<Input
-						placeholder="Search thesis, group, supervisor"
-						style={{ width: 300 }}
-						prefix={<SearchOutlined />}
-					/>
-					<div className="flex gap-2">
-						<Select defaultValue="Summer 2025" style={{ width: 130 }}>
-							<Select.Option value="Summer 2025">Summer 2025</Select.Option>
-						</Select>
-						<Button icon={<DownloadOutlined />}>Export Statistic</Button>
-					</div>
-				</div>
-				<Table
-					columns={columns}
-					dataSource={groupTableData}
-					pagination={{ pageSize: 5 }}
-					rowKey="groupName"
+			<Space direction="vertical" size="small" style={{ width: '100%' }}>
+				<Title level={4} style={{ margin: 0 }}>
+					Groups Table
+				</Title>
+				<Text type="secondary">
+					List of student groups that have selected and been assigned thesis
+					topics.
+				</Text>
+			</Space>
+
+			<div className="flex justify-between items-center mb-4">
+				<Input
+					placeholder="Search thesis, group, supervisor"
+					style={{ width: 300 }}
+					prefix={<SearchOutlined />}
 				/>
+				<div className="flex gap-2">
+					<Select defaultValue="Summer 2025" style={{ width: 130 }}>
+						<Select.Option value="Summer 2025">Summer 2025</Select.Option>
+					</Select>
+					<Button icon={<DownloadOutlined />}>Export Statistic</Button>
+				</div>
 			</div>
+			<Table
+				columns={columns}
+				dataSource={groupTableData}
+				pagination={{ pageSize: 5 }}
+				rowKey="groupName"
+			/>
 		</Card>
 	);
 }
