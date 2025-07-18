370bb1b4e261f9feb7b496be95f43447f076431c
sidde1512 <binhbobinhbo22@gmail.com>
2025-07-18 09:37:50 +0700

    feat: add ChecklistEdit and ChecklistDetail pages with metadata

--- src/app/(dashboard)/lecturer/checklist-edit/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function ChecklistEditRedirectPage() {
-	// This page is now deprecated - redirect to use the new [id] dynamic route
-	redirect('/lecturer/checklist-management');
-}
