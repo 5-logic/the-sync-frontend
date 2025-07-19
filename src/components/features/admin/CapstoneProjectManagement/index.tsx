c40739a698c3e0709ac86b158639f51500c9c49d
Yuhtnguyen <nguyenthithuy1022003@gmail.com>
2025-07-19 21:45:28 +0700

    feat: add MyThesisSection component for lecturer dashboard

--- src/components/features/admin/CapstoneProjectManagement/index.tsx
+++ src/components/features/admin/CapstoneProjectManagement/index.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
 	return (
 		<Space direction="vertical" size="large" style={{ width: '100%' }}>
 			<Header
-				title="Capstone Project Management"
+				title="Capstone Project"
 				description="Capstone Project Management is a platform that updates supervision assignment decisions and facilitates comprehensive tracking and management of capstone project implementation."
 				badgeText="Summer 2025 - FPT University Binh Dinh"
 			/>
