6cad92409b6cb4e383d37ff8458422376978cc18
Yuhtnguyen <nguyenthithuy1022003@gmail.com>
2025-07-21 09:12:11 +0700

    Revert "fix: correct title and description in CapstoneDefenseResults metadata"
    This reverts commit 1209be0dd7ec48738ae434bcf7b66362b0959d3f.

--- src/app/(dashboard)/admin/capstone-defense-results/page.tsx
+++ src/app/(dashboard)/admin/capstone-defense-results/page.tsx
@@ -2,11 +2,11 @@ import { createMetadata } from '@/app/metadata';
 import CapstoneDefenseResults from '@/components/features/admin/CapstoneDefenseResults';
 
 export const metadata = createMetadata({
-	title: 'Admin Capstone Defense Results',
+	title: 'Admin Capstone Project Management',
 	description:
-		'Admin Capstone Defense Results for TheSync - Group Formation and Capstone Thesis Development',
+		'Admin Capstone Project Management for TheSync - Group Formation and Capstone Thesis Development',
 });
 
-export default function CapstoneDefenseReFsultsPage() {
+export default function CapstoneDefenseResultsPage() {
 	return <CapstoneDefenseResults />;
 }
