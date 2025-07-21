d208c22d19928bf1d43b8ff6a09225117cd233b7
Yuhtnguyen <nguyenthithuy1022003@gmail.com>
2025-07-21 15:19:13 +0700

    fix: remove outdated file path comment from FilterBar component

--- src/components/features/admin/CapstoneProjectManagement/FilterBar.tsx
+++ src/components/features/admin/CapstoneProjectManagement/FilterBar.tsx
@@ -1,4 +1,3 @@
-// components/common/FilterBar.tsx
 import { ExportOutlined, SearchOutlined } from '@ant-design/icons';
 import { Button, Col, Input, Row, Select } from 'antd';
 import React from 'react';
