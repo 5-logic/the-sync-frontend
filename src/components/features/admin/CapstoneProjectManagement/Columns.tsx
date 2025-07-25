e430d755a606e1fbd2d92ff3f36a47ec5de273c0
sidde1512 <binhbobinhbo22@gmail.com>
2025-07-25 12:37:14 +0700

    refactor: update GroupTableData import to use store module

--- src/components/features/admin/CapstoneProjectManagement/Columns.tsx
+++ src/components/features/admin/CapstoneProjectManagement/Columns.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 import { highlightText } from '@/components/features/admin/CapstoneProjectManagement/HighlightText';
 import { RowSpanCell } from '@/components/features/admin/CapstoneProjectManagement/RowSpanCell';
-import { GroupTableData } from '@/components/features/admin/CapstoneProjectManagement/useGroupTableData';
+import { type GroupTableData } from '@/store';
 
 // Dùng chung cho cả bảng GroupResults và GroupManagement
 export const getColumns = (
